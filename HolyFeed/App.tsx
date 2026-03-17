5676e637b0ba3898604a168df8d5890491649996
haeunwooood <haeunwooood@gmail.com>
2026-03-17 13:35:09 +0900

    로딩바 색상 변경

--- HolyFeed/App.tsx
+++ HolyFeed/App.tsx
@@ -184,7 +184,7 @@ export default function App() {
   if (isInitializing) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#000" />
       </View>
     );
   }
